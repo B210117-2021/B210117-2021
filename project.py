6f018e4cc3b8d6774aa71f14aa4dea7c891241f6
Peixuan Li <s2194601@bioinfmsc5.bio.ed.ac.uk>
2021-11-29 18:44:59 +0000

    Adding the functiothe level of protein sequence conservation

--- project.py
+++ project.py
@@ -21,11 +21,15 @@ subprocess.call('chmod 700 project.py',shell = True)
 
 ##This section is generated to conduct multiple alignment among desired sequences with clustalo installed in MSc server
 
-subprocess.call("clustalo -i sequence_required.fasta -o multi_alig.fasta",shell = True)
+#subprocess.call("clustalo -i sequence_required.fasta -o multi_alig.fasta",shell = True)
 
+##This section is generaterd to plot the the level of protein sequence conservation across the species within that taxonomic group
 
+win_size = input("Please input a window size (note:Number of columns to average alignment quality over. The larger this value is, the smoother the plot will be. (Any integer value))\n\t")
+graph_type = input("Please type in  an output graph type you want (Options: ps, hpgl, hp7470, hp7580, meta, cps, x11, tek, tekt, none, data, xterm, svg)\n\t")
 
-
+command_plot = "plotcon -sequence multi_alig.fasta -winsize " + str(win_size) + " -graph " + graph_type 
+subprocess.call(command_plot,shell = True)
 
 
 

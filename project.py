340ac0474f943943305001a6dc270dc7f5e054cd
Peixuan Li <s2194601@bioinfmsc5.bio.ed.ac.uk>
2021-12-03 15:49:27 +0000

    fianl

--- project.py
+++ project.py
@@ -7,18 +7,19 @@ a = input('please enter the protein family:\n\t')
 b = input('please enter the taxonomic group:\n\t')
 
 import subprocess
-subprocess.call('chmod 700 project.py',shell = True)
 
 #######################################################################################################################
 ##This section is generated to search for the desired protein and organisms from NCBI database
 
 #A default query sentence of esearch is made, and replace the default word with user input
-#default_line = "esearch -db protein -query '99[TITL]'| efilter -query ''66'[ORGN] AND 0:1000[SLEN]'| efetch -format fasta > sequence_required.fasta"
-#command_line_1 = default_line.replace('99',a)
-#command_line = command_line_1.replace('66',b)
+default_line = "esearch -db protein  -query '99[TITL]'| efilter -query '66[organism]'| efetch -format fasta > sequence_required.fasta"
+command_line_1 = default_line.replace('99',a)
+command_line = command_line_1.replace('66',b)
 
 #os.system is used to run esearch command line in py transcipt
-#os.system(command_line)
+
+subprocess.call(command_line,shell = True)
+
 
 #######################################################################################################################
 ##This section of code is generated a report about what species are shown dataset
@@ -77,7 +78,7 @@ del(line)
 #######################################################################################################################
 ##This section is generated to conduct multiple alignment among desired sequences with clustalo installed in MSc server
 
-#subprocess.call("clustalo -i sequence_required.fasta -o multi_alig.fasta",shell = True)
+subprocess.call("clustalo -i sequence_required.fasta -o multi_alig.fasta",shell = True)
 
 ##This section is generaterd to plot the the level of protein sequence conservation across the species within that taxonomic group
 
